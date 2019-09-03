959f783a08d6db25c6a93887b9e6c177ea676332
Yu Chen <ychennay@gmail.com>
2019-09-03 09:46:44 -0700

    add tagging for facial expressions (smiling / not smiling)

--- data_preprocessing/tagging.py
+++ data_preprocessing/tagging.py
@@ -2,7 +2,7 @@ import boto3
 import os
 from core_config import ROOT_DIR
 from enum import Enum
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Type
 import re
 import json
 
@@ -13,6 +13,9 @@ class Genders(Enum):
     MALE = "male"
     FEMALE = "female"
 
+class FacialExpressions(Enum):
+    SMILING = "smiling"
+    NOT_SMILING = "not_smiling"
 
 class MetadataStore:
     classification_dictionary: Dict[str, Dict[str, Any]] = {}
@@ -25,34 +28,36 @@ S3_BUCKET_NAME = "fei-faces-sao-paulo"
 LOCAL_JSON_FILENAME = "gender.json"
 
 
-def populate_classification_dictionary() -> None:
-    absolute_path: Path = os.path.join(ROOT_DIR, "face_images", "classification_tags", "gender")
-    for gender in Genders:
-        gender_directory: Path = os.path.join(absolute_path, gender.value)
-        image_names: List[str] = os.listdir(gender_directory)
+def populate_classification_dictionary( dimension_enums: Type[Enum], dimension: str = "gender") -> None:
+    absolute_path: Path = os.path.join(ROOT_DIR, "face_images", "classification_tags", dimension)
+    for dimension_enum in dimension_enums:
+        dimension_directory: Path = os.path.join(absolute_path, dimension_enum.value)
+        image_names: List[str] = os.listdir(dimension_directory)
         for image_name in image_names:
             match = re.search(IMAGE_NAME_REGEX, image_name)
             if match:
                 subject_id: str = match.group(1)
                 if subject_id not in MetadataStore.classification_dictionary.keys():
-                    MetadataStore.classification_dictionary[image_name] = gender.value
+                    MetadataStore.classification_dictionary[image_name] = dimension_enum.value
             else:
                 easy_match = re.search(EASY_IMAGE_NAME_REGEX, image_name)
                 assert easy_match, f"{image_name} was not a match for any known filename patterns."
                 subject_id: str = easy_match.group(1)
                 if subject_id not in MetadataStore.easy_classification_dictionary.keys():
-                    MetadataStore.easy_classification_dictionary[image_name] = gender.value
+                    MetadataStore.easy_classification_dictionary[image_name] = dimension_enum.value
+    print(MetadataStore.classification_dictionary)
 
 
 if __name__ == "__main__":
-    populate_classification_dictionary()
+    dimension = "smiling"
+    easy = False
+
+    LOCAL_JSON_FILENAME = f"{dimension}.json"
+
+    populate_classification_dictionary(FacialExpressions, dimension=dimension)
 
     with open(LOCAL_JSON_FILENAME, "w") as json_file:
         json.dump(MetadataStore.classification_dictionary, json_file)
-
-    easy_json_filename = "easy_" + LOCAL_JSON_FILENAME
-    with open(easy_json_filename, "w") as easy_json_file:
-        json.dump(MetadataStore.easy_classification_dictionary, easy_json_file)
 
     s3 = boto3.resource('s3')
     faces_bucket = s3.Bucket(S3_BUCKET_NAME)
@@ -60,5 +65,10 @@ if __name__ == "__main__":
     faces_bucket.upload_file(LOCAL_JSON_FILENAME, f"classification/{LOCAL_JSON_FILENAME}")
     print(f"Uploaded {LOCAL_JSON_FILENAME}")
 
-    faces_bucket.upload_file(easy_json_filename, f"classification/{easy_json_filename}")
-    print(f"Uploaded {easy_json_filename}")
+    if easy:
+        easy_json_filename = "easy_" + LOCAL_JSON_FILENAME
+        with open(easy_json_filename, "w") as easy_json_file:
+            json.dump(MetadataStore.easy_classification_dictionary, easy_json_file)
+
+        faces_bucket.upload_file(easy_json_filename, f"classification/{easy_json_filename}")
+        print(f"Uploaded {easy_json_filename}")
